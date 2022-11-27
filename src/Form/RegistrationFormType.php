b35581ba128b658e7469932d1c97470004250d58
Arkang67 <arthur.entrepreneuriat@hotmail.com>
2022-11-27 23:01:45 +0100

    découverte symfony login/register/role_admin

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('pseudo', TextType::class)
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                // La valeur ne sera pas stocké en bdd
+                'mapped' => false,
+                // Type de la valeur : password
+                'type' => PasswordType::class,
+                // Si les 2 mdp ne correspondent pas -> renvoie le message suivant
+                'invalid_message' => 'The password fields must match',
+                // Donne la classe password-field au input
+                'options' => ['attr' => ['class' => 'password-field']],
+                // Obligé de remplire le champs.
+                'required' => true,
+                // Première option le mdp
+                'first_options' => ['label' => 'Password'],
+                // Deuxième option : la vérification du mdp
+                'second_options' => ['label' => 'Repeat Password'],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
